6df0be92150520f242436961e40f815eda784fa5
Jessy Song <46058482+Jessyjias@users.noreply.github.com>
2024-05-14 21:28:28 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,180 @@
+import streamlit as st
+from st_copy_to_clipboard import st_copy_to_clipboard
+from streamlit_echarts import st_echarts
+from streamlit_extras.grid import grid 
+from annotated_text import annotated_text
+import leafmap.foliumap as leafmap
+from utils_ctg import *
+from markdownlit import mdlit
+import pandas as pd
+import numpy as np
+
+# from matplotlib.backends.backend_agg import RendererAgg
+# _lock = RendererAgg.lock
+
+
+# -- Set page config
+apptitle = 'TrialExplore'
+
+st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
+
+## -- set some cache functions 
+
+## -- set some states 
+if "next_trial_clicked" not in st.session_state:
+    st.session_state.next_trial_clicked = False
+if 'form_submit_clicked' not in st.session_state:
+    st.session_state.form_submit_clicked = False
+if 'df_ct' not in st.session_state:
+    st.session_state.df_ct = pd.DataFrame()
+if 'trial_index' not in st.session_state:
+    st.session_state.trial_index = 0 
+if 'search_params' not in st.session_state:
+    st.session_state.search_params = []
+
+## some css modifications 
+st.markdown("""
+    <style>
+        .stMultiSelect [data-baseweb=select] span{
+            max-width: 300px;
+            font-size: 0.8rem;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+# button callbacks 
+def callback_nexttrial():
+    st.session_state.next_trial_clicked = True
+def callback_formsubmit():
+    st.session_state.form_submit_clicked = True
+
+def main():
+
+    # <------------- side bar --------------> 
+    sb = st.sidebar
+
+    sb.title('Get Trial Records Settings')
+    sb_expander = sb.expander("Search Parameters", expanded=True)
+    ctg_search_form = sb_expander.form("search_trials_form")
+    
+    input_condition = ctg_search_form.text_input(
+    "Condition/Disease  \n Ex) Breast Cancer",
+    help="Describe the disease/condition. Ex. Breast cancer",
+    key='input_condition'
+    )
+    input_intr = ctg_search_form.text_input(
+        "Type of Intervention  \n Ex) Drug, medical device...",
+        help='Describe the type of intervention in the trial. This can be general such as limiting search involving a drug by inputing "DRUG", or entering a specific drug name.',
+        key='input_intr'
+    )
+    input_loc = ctg_search_form.text_input(
+        "Location  \n Ex) US, San Francisco, or California",
+        help='This can be a country name, state name or city name.',
+        key='input_loc'
+    )
+    multiselect_status = ctg_search_form.multiselect(
+        "Status of the trial  \n Ex) Recruiting, Invitation needed...",
+        ['RECRUITING', 'ENROLLING_BY_INVITATION','NOT_YET_RECRUITING','ACTIVE_NOT_RECRUITING', 'COMPLETED', 'SUSPENDED', 'TERMINATED', 'WITHDRAWN', 'AVAILABLE', 'NO_LONGER_AVAILABLE', 'TEMPORARILY_NOT_AVAILABLE', 'APPROVED_FOR_MARKETING', 'WITHHELD', 'UNKNOWN'],
+        default=['RECRUITING', 'ENROLLING_BY_INVITATION'],
+        help='Filter results by the trial status. Default to presenting only recruiting trials, whether it is by invite or open to public.', 
+        key='multiselect_status'
+    )
+
+    form_submit = ctg_search_form.form_submit_button(label="Submit", on_click=callback_formsubmit)
+    sb.divider()
+    saved_trials = sb.title('Saved trial IDs')
+    sb.subheader('Copy and paste for later') 
+                        # on_click=get_ctg_records, 
+                        # args=(input_condition, input_intr, input_loc, multiselect_status), 
+                        # kwargs=None,use_container_width=False)
+    if st.session_state.search_params == []:
+        st.session_state.search_params = [input_condition, input_intr, input_loc, multiselect_status]
+    
+    # <------------- calling api for getting results: first search or when parameters change --------------> 
+    if (st.session_state.df_ct.empty) or st.session_state.search_params != [input_condition, input_intr, input_loc, multiselect_status]: 
+        if (form_submit or st.session_state.form_submit_clicked): 
+            sb.divider()
+            sb.write(input_condition, input_intr, input_loc, multiselect_status)
+            # <------------- main section display --------------> 
+            with st.spinner("Pulling data... (if it takes too long, try to put in more detailed search parameters. )"):
+                df_res = get_ctg_records(input_condition, input_intr, input_loc, multiselect_status)
+                st.session_state.df_ct = df_res
+            
+            st.session_state.trial_index = 0
+            st.session_state.search_params = [input_condition, input_intr, input_loc, multiselect_status]
+            # st.session_state.form_submit_clicked = False
+            
+
+    # <------------- display after the dataframe is stored in state --------------> 
+    if not st.session_state.df_ct.empty: 
+        df = st.session_state.df_ct
+    # <------------- main section display: sec 1 analytics --------------> 
+        # Graphs
+        st.header('Data Analytics', divider='rainbow')
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1:
+            data_st = df['Study Type'].value_counts().to_dict()
+            st.header('Study Type')
+            st_echarts(options=get_pie_graph_options('Study Types', data=data_st), height="400px")
+        with col2:
+            df_p = df['Phases'].value_counts().reset_index(name='counts')
+            non_phases_terms = [term for term in df_p['Phases'].values if 'phase' not in term.lower()]
+            df_p.loc[len(df_p.index)] = ['N/A', df_p[df_p['Phases'].isin(non_phases_terms)].counts.sum()] 
+            df_p = df_p[~df_p['Phases'].isin(non_phases_terms)]
+            df_p = pd.Series(df_p.counts.values,index=df_p.Phases).to_dict()
+            st.header('Study Phase')
+            st_echarts(options=get_pie_graph_options('Study Phases', data=df_p), height="450px")
+        with col3:
+            st.header('Recruiting Sex')
+            df_s = df['Sex'].dropna().value_counts().to_dict()
+            st_echarts(options=get_pie_graph_options('Sex', data=df_s), height="400px")
+
+        # Map 
+        df_locs_map = get_locations_df(df)
+        # with st.expander('map'):
+        st.map(df_locs_map,
+            latitude='lat',
+            longitude='lng',
+            size='counts')
+        
+
+    if not st.session_state.df_ct.empty: 
+        # <------------- main section display: sec 2 available data + explore in flashcard type view --------------> 
+        st.header('Trials', divider='rainbow')
+        st.dataframe(data=df)
+        df = st.session_state.df_ct
+        with st.expander('Find Trial Details', expanded=True): 
+            next_trial = st.button(
+            "Next trial", on_click=callback_nexttrial, key="next_trial", use_container_width=True) 
+            if next_trial or st.session_state.next_trial_clicked:
+                st.session_state.trial_index += 1
+                st.session_state.next_trial_clicked = False
+            
+            cur_row = df.loc[st.session_state.trial_index]
+            print(st.session_state.trial_index)
+            st.markdown(
+                f'<h2><span style="color:black"> \
+                    {cur_row.briefTitle}</span></h2> ', 
+                # <h4>&mdash; Trial index no. {st.session_state.trial_index}</em></h4></div></div>'
+                unsafe_allow_html=True,
+            )
+            # print(cur_row['Interventions'], )
+            annotated_text((str(st.session_state.trial_index), "form index", "#CEE6F2"), '   ',
+                            (cur_row['NCT ID'], "NCT ID", "#CEE6F2"), '  \n',
+                            [(intervent, "Interventions", "#6AB187") for intervent in cur_row['Interventions'].split(', ')], '  \n',
+                        #    (cur_row['Interventions'], "Interventions", "#6AB187"), '  \n',
+                        [(cond, "condition", "#FFBB00") for cond in cur_row['Conditions'].split(', ')], '  \n')
+            mdlit('#### Brief Summary')
+            mdlit(cur_row['briefSummary'])
+            mdlit('#### Eligibility Criteria')
+            mdlit(cur_row['Eligibility Criteria'])
+
+        
+            # mdlit(cur_row[''])
+            # print([cond for cond in cur_row['Conditions'].split(', ')])
+
+                # print('st.session_state.df_ct.empty', st.session_state.df_ct.empty)
+            
+
+
+main()
---
+++ utils_ctg.py
@@ -0,0 +1,159 @@
+import requests
+import pandas as pd
+
+
+def get_ctg_records(cond, intr, locn, status):
+    # Initial URL for the first API call
+    base_url = "https://clinicaltrials.gov/api/v2/studies"
+    params = {
+        "query.cond": cond,
+        "query.intr": intr,
+        'query.locn': locn, 
+        'filter.overallStatus':  '|'.join(status) ,
+        "pageSize": 100
+    }
+
+    # Initialize an empty list to store the data
+    data_list = []
+
+    # Loop until there is no nextPageToken
+    while True:
+        # Print the current URL (for debugging purposes)
+        print("Fetching data from:", base_url + '?' + '&'.join([f"{k}={v}" for k, v in params.items()]))
+        
+        # Send a GET request to the API
+        response = requests.get(base_url, params=params)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            data = response.json()  # Parse JSON response
+            studies = data.get('studies', [])  # Extract the list of studies
+
+            # Loop through each study and extract specific information
+            for study in studies:
+                # Safely access nested keys
+                nctId = study['protocolSection']['identificationModule'].get('nctId', 'Unknown')
+                briefTitle = study['protocolSection']['identificationModule'].get('briefTitle', 'Unknown')
+                officialTitle = study['protocolSection']['identificationModule'].get('OfficialTitle', 'Unknown')
+                briefSummary = study['protocolSection']['descriptionModule'].get('briefSummary', 'Unknown')
+                overallStatus = study['protocolSection']['statusModule'].get('overallStatus', 'Unknown')
+                startDate = study['protocolSection']['statusModule'].get('startDateStruct', {}).get('date', 'Unknown Date')
+                conditions = ', '.join(study['protocolSection']['conditionsModule'].get('conditions', ['No conditions listed']))
+                acronym = study['protocolSection']['identificationModule'].get('acronym', 'Unknown')
+
+                # Extract interventions safely
+                interventions_list = study['protocolSection'].get('armsInterventionsModule', {}).get('interventions', [])
+                interventions = ', '.join([intervention.get('name', 'No intervention name listed') for intervention in interventions_list]) if interventions_list else "No interventions listed"
+                
+                # Extract locations safely
+                locations_list = study['protocolSection'].get('contactsLocationsModule', {}).get('locations', [])
+                # location_facilities = 
+                locations = ', '.join([f"{location.get('city', 'No City')} / {location.get('state', 'No State')} / {location.get('country', 'No Country')}" for location in locations_list]) if locations_list else "No locations listed"
+                
+                # Extract contact info 
+                centralContact = study['protocolSection'].get('contactsLocationsModule', {}).get('centralContacts', [])
+                contacts = ', '.join([f"{contact.get('name', 'No Name')} -  {contact.get('role', 'No Role')} - {contact.get('phone', 'No Phone')} - {contact.get('phoneExt', 'No phoneExt')} - {contact.get('email', 'No email')}" for contact in centralContact]) if centralContact else "No contact listed"
+
+                # Extract dates and phases
+                primaryCompletionDate = study['protocolSection']['statusModule'].get('primaryCompletionDateStruct', {}).get('date', 'Unknown Date')
+                studyFirstPostDate = study['protocolSection']['statusModule'].get('studyFirstPostDateStruct', {}).get('date', 'Unknown Date')
+                lastUpdatePostDate = study['protocolSection']['statusModule'].get('lastUpdatePostDateStruct', {}).get('date', 'Unknown Date')
+                studyType = study['protocolSection']['designModule'].get('studyType', 'Unknown')
+                phases = ', '.join(study['protocolSection']['designModule'].get('phases', ['Not Available']))
+
+                # Extract eligibility requirements safely 
+                eligibilityCriteria = study['protocolSection']['eligibilityModule'].get('eligibilityCriteria', {})
+                sex = study['protocolSection']['eligibilityModule'].get('sex', {})
+                genderDescription = study['protocolSection']['eligibilityModule'].get('genderDescription', 'Unknown')
+                minimumAge = study['protocolSection']['eligibilityModule'].get('minimumAge', 'Unknown')
+                maximumAge = study['protocolSection']['eligibilityModule'].get('maximumAge', 'Unknown')
+                # phases = ', '.join(study['protocolSection']['designModule'].get('phases', ['Not Available']))
+
+                # Append the data to the list as a dictionary
+                data_list.append({
+                    "NCT ID": nctId,
+                    "Acronym": acronym,
+                    "briefTitle": briefTitle, 
+                    # "officialTitle": officialTitle, 
+                    "briefSummary": briefSummary, 
+                    "Overall Status": overallStatus,
+                    "Start Date": startDate,
+                    "Conditions": conditions,
+                    "Interventions": interventions,
+                    "Locations": locations,
+                    "Contacts": contacts, 
+                    "Primary Completion Date": primaryCompletionDate,
+                    "Study First Post Date": studyFirstPostDate,
+                    "Last Update Post Date": lastUpdatePostDate,
+                    "Study Type": studyType,
+                    "Phases": phases,
+                    
+                    # 
+                    "Eligibility Criteria": eligibilityCriteria,
+                    "Sex": sex,
+                    # "Gender": genderDescription,
+                    "Min Age": minimumAge,
+                    "Max Age": maximumAge,
+                })
+
+            # Check for nextPageToken and update the params or break the loop
+            nextPageToken = data.get('nextPageToken')
+            if nextPageToken:
+                params['pageToken'] = nextPageToken  # Set the pageToken for the next request
+            else:
+                break  # Exit the loop if no nextPageToken is present
+        else:
+            print("Failed to fetch data. Status code:", response.status_code, response.text)
+
+            ### TODO: return a default dataframe? 
+            break
+
+    if len(data_list)>0: 
+        df = pd.DataFrame(data_list)
+    else: 
+        df = pd.read_csv("./data/clinical_trials_data_search_res.csv")
+
+    print('got all records', df.shape)
+    return df
+
+
+def get_pie_graph_options(name, data): 
+    options = {
+        "tooltip": {"trigger": "item"},
+        "legend": {"top": "5%", "left": "center"},
+        "series": [
+            {
+                "name": name,
+                "type": "pie",
+                "radius": ["40%", "70%"],
+                "avoidLabelOverlap": False,
+                "itemStyle": {
+                    "borderRadius": 10,
+                    "borderColor": "#fff",
+                    "borderWidth": 2,
+                },
+                "label": {"show": False, "position": "center"},
+                "emphasis": {
+                    "label": {"show": True, "fontSize": "15", "fontWeight": "bold"}
+                },
+                "labelLine": {"show": False},
+                "data": [{"value": val, "name": n} for n,val in data.items()]
+            }
+        ],
+    }
+
+    return options 
+
+def get_locations_df(df): 
+    cities_list = [cities.split(', ') for cities in df['Locations'].values.tolist()]
+    cities_list = [city for cities in cities_list for city in cities if '/' in city and len(city.split(' / '))==3]
+    cities = [city.split(' / ')[0] for city in cities_list]
+    states = [city.split(' / ')[1] for city in cities_list]
+    countries = [city.split(' / ')[2] for city in cities_list]
+    df_locs = pd.DataFrame(data={'city':cities, 'state':states, 'country':countries})
+    df_locs = df_locs.value_counts().reset_index(name='counts')
+    df_cities_geo = pd.read_csv('./data/worldcities.csv')
+    df_locs_formap = pd.merge(df_locs, df_cities_geo[['city', 'lat', 'lng', 'admin_name', 'country']], left_on=['city', 'state', 'country'], right_on=['city', 'admin_name', 'country'], how='inner')
+    df_locs_formap['counts'] = df_locs_formap['counts']*100
+    # print(df_locs_formap)
+    return df_locs_formap
